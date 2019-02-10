6471a41bda6bb4cfd972103964f688a56a07934e
Finn Kiesinger <fkiesinger@me.com>
2019-02-10 21:01:26 +0100

    created sidebar and gradient

---
+++ Smack/Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Finn Kiesinger on 10.02.19.
+//  Copyright © 2019 Finn Kiesinger. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
