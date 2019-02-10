53aa76760e8ae263880debedad3d4da42aef020a
Finn Kiesinger <fkiesinger@me.com>
2019-02-10 19:16:29 +0100

    added sidebar with SWReveal

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  Smack
 //
 //  Created by Finn Kiesinger on 10.02.19.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
     }
-
+    
+    
 
 }
-
---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Finn Kiesinger on 10.02.19.
+//  Copyright © 2019 Finn Kiesinger. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer((self.revealViewController()?.tapGestureRecognizer())!)
+        
+    }
+
+
+}
